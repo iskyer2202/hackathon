4bedaf7afbe33269e168daf13ee47d9d5adcb6de
Dasheng Wang <skyer2202@outlook.com>
2023-05-31 18:26:06 +0800

    first commit

---
+++ src/client/apis.ts
@@ -0,0 +1,23 @@
+export default {
+  baseUrl: 'https://api.anthropic.com',
+  endpoints: {
+    v1: {
+      completions: {
+        url: '/v1/completions',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      chat: {
+        completions: {
+          url: '/v1/chat/completions',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      },
+    },
+  },
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { Navbar } from 'react-daisyui';
+import { useTranslation } from 'react-i18next';
+import { BsGithub, BsTwitter } from 'react-icons/bs';
+
+import { ConfigButton } from '@/components/ConfigButton';
+import { SwitchLanguageButton } from '@/components/SwitchLanguageButton';
+import { ToggleThemeButton } from '@/components/ToggleThemeButton';
+
+function AboutModal() {
+  return (
+    <>
+      <input type="checkbox" id="about-modal" className="modal-toggle" />
+      <label htmlFor="about-modal" className="cursor-pointer modal">
+        <label className="relative modal-box" htmlFor="">
+          <h3 className="text-lg font-bold">About Professional Translator</h3>
+          <p className="grid grid-cols-2 gap-2 py-4">
+            <a
+              href="https://github.com/skyer2202/translator"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="btn btn-outline"
+            >
+              <BsGithub size={20} className="mr-2" />
+              GitHub
+            </a>
+          </p>
+        </label>
+      </label>
+    </>
+  );
+}
+
+function Header() {
+  const { t } = useTranslation();
+  return (
+    <>
+      <AboutModal />
+      <Navbar className="sticky top-0 z-50 bg-primary text-primary-content">
+        <div className="flex-1">
+          <label className="text-xl normal-case btn btn-ghost" htmlFor="about-modal">
+            {t('topbar.title')}
+          </label>
+        </div>
+        <div className="flex-none">
+          <ConfigButton />
+          <ToggleThemeButton />
+          <SwitchLanguageButton />
+        </div>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
